a0115f3943c5676e25abdc1aaa4e6e14915da3ec
Behnam Saeedi <Behnam Saeedi>
2017-03-21 22:23:01 -0700

    test for simple expressive

--- parser.cpp
+++ parser.cpp
@@ -529,8 +529,10 @@ struct TreeNode * Parser::expression()
 {
 	currentToken = nextToken();
 	if (DEBUG)
-		std::cout << "-> before if Token at line " << currentToken.line << ": " << currentToken.str << "." << std::endl;
+		std::cout << "-> before if 1 Token at line " << currentToken.line << ": " << currentToken.str << "." << std::endl;
 	currentToken = nextToken();
+	if (DEBUG)
+		std::cout << "-> before if 2 Token at line " << currentToken.line << ": " << currentToken.str << "." << std::endl;
 	if(currentToken.str[0] != '=')// it is the = it returns
 	{
 		tokenIndex--;
