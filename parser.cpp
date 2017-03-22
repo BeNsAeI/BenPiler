54f56018f75086874a3f2a6b92c0807836fadf88
Behnam Saeedi <Behnam Saeedi>
2017-03-22 06:00:47 -0700

    fixing the unexpected token error

--- parser.cpp
+++ parser.cpp
@@ -147,7 +147,7 @@ struct TreeNode * Parser::declaration()
 			if(semi.str[0] != ';')
 			{
 				printf(ANSI_COLOR_RED "error " ANSI_COLOR_RESET "at line " ANSI_COLOR_CYAN "%d: " ANSI_COLOR_RESET,semi.line);
-				std::cout << "\"" << semi.str << "\"" << " Unexpected token. \";\" is missing." << std::endl;
+				std::cout << "\"" << semi.str << "\"" << " Unexpected tokenu. \";\" is missing." << std::endl;
 				exit(-1);
 			}
 			break;
@@ -419,9 +419,6 @@ struct TreeNode * Parser::statement()
 	}
 	else if(currentToken.type == NONTOKEN)
 	{
-		tokenIndex=Tokens.end();
-		tokenIndex--;
-		currentToken = nextToken();
 		return NULL;
 	}
 	else
