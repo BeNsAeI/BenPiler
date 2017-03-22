6c8e2aa7f6fa690afc59102a583adab361cc4a33
Behnam Saeedi <Behnam Saeedi>
2017-03-22 05:22:59 -0700

    fixing the unexpected token error

--- parser.cpp
+++ parser.cpp
@@ -514,8 +514,6 @@ struct TreeNode * Parser::expression()
 	currentToken = nextToken();
 	if (DEBUG)
 		std::cout << "-> before if 1 Token at line " << currentToken.line << ": " << currentToken.str << "." << std::endl;
-	if (currentToken.str[0] == '}')
-		return NULL;
 	currentToken = nextToken();
 	if (DEBUG)
 		std::cout << "-> before if 2 Token at line " << currentToken.line << ": " << currentToken.str << "." << std::endl;
