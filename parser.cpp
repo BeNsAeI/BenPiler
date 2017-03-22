d4358e081fffdc7a6205c99a4cb5732d296a5b59
Behnam Saeedi <Behnam Saeedi>
2017-03-22 06:16:02 -0700

    fixing the unexpected token error

--- parser.cpp
+++ parser.cpp
@@ -284,7 +284,11 @@ struct TreeNode * Parser::compound_stmt()
 	node->c1 = node->sibling;
 	node->sibling = NULL;
 	node->c2 = statement_list();
-	//currentToken = nextToken();
+	if (currentToken.type == NONTOKEN)
+	{
+		tokenIndex = Tokens.end();
+		tokenIndex--;
+	}
 	if(currentToken.str[0] != '}')
 	{
 		printf(ANSI_COLOR_RED "error " ANSI_COLOR_RESET "at line " ANSI_COLOR_CYAN "%d: " ANSI_COLOR_RESET,currentToken.line);
@@ -423,12 +427,7 @@ struct TreeNode * Parser::statement()
 	}
 	else
 	{
-		if (currentToken.type == NONTOKEN)
-		{
-			tokenIndex=Tokens.end();
-			tokenIndex--;
-		}
-		if (currentToken.str[0] == ';' || currentToken.str[0] == '}')
+		if (currentToken.str[0] == ';' || currentToken.str[0] == '}' || currentToken.type == NONTOKEN)
 		{
 			return statement();
 		}
