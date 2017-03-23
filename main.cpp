e0e783c0a5374e01e91cde6c2c865aaf79e8b255
Behnam Saeedi <Behnam Saeedi>
2017-03-22 21:23:25 -0700

    Code cleaning

--- main.cpp
+++ main.cpp
@@ -42,6 +42,7 @@ int main (int argc, char ** argv)
 	}
 	Parser * myParser = new Parser(myScanner->Tokens,DEBUG);
 	struct TreeNode * AST = myParser->Parse();
+	myParser->Print();
 	cout << "cleaning out..." << endl;
 	delete (myParser);
 	delete (myScanner);
